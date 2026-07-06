98edc2e8c009aeb60da6546f7e6e88c47d498144
Ayush Pundir <ayushpundir12112@gmail.com>
2026-07-06 22:12:02 +0530

    chages order

--- src/Components/Topheader.jsx
+++ src/Components/Topheader.jsx
@@ -9,8 +9,8 @@ function Topheader() {
   return (
     <header className="top-header">
       <ul>
-        <li><NavLink to="/projects">Projects</NavLink></li>
         <li><NavLink to="/experience">Experience</NavLink></li>
+        <li><NavLink to="/projects">Projects</NavLink></li>
         <li><NavLink to="/certifications">Certifications</NavLink></li>
         <li>
           <button
